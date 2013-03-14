cee28e8e45a4a58d96ba437465d9e132cdeda9a2
mike <mike@astral>
2013-03-14 23:52:33 +0700

    initial

---
+++ src/ru/mcterra/cluster/ClusterTools.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.mcterra.cluster;
+
+/**
+ *
+ * @author mike
+ */
+public class ClusterTools {
+    
+    
+    
+    
+}
